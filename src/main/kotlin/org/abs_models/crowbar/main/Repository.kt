f3bb5f24ff44b299018cbc0b5f42315e80e3b5be
scal9000 <scalettamarco@yahoo.it>
2022-07-22 15:15:10 +0200

    feature(support for "readln", "intToString")

--- src/main/kotlin/org/abs_models/crowbar/main/Repository.kt
+++ src/main/kotlin/org/abs_models/crowbar/main/Repository.kt
@@ -246,7 +246,7 @@ object FunctionRepos{
 		"fstT", "sndT","trdT", //triple
 		"contains", "set", "insertElement", "keys",//set
 		"emptyMap", "lookup", "map", //map
-		"println", "toString" //String
+		"println", "toString", "intToString", "readln" //String
 	)
 	val known : MutableMap<String, FunctionDecl> = mutableMapOf()
 	val simpleNameFunctions : MutableSet<String> = mutableSetOf()
